102fbc35ec3ad3df120d7fff0dd96197c38fcf53
Franklin Waller <franklin.dijks@gmail.com>
2014-11-01 16:50:53 +0100

    Building up structure for loading views. input, content and tab bar styling

--- js/playui.js
+++ js/playui.js
@@ -1,7 +1,37 @@
-(function(global){
-    //Use the global variable to attach an object to the window object.
-    
-    
-    
-    
+(function(global) {
+//Use the global variable to attach an object to the window object.
+
+    function init() {
+        var tabbar = document.getElementsByClassName('play-tabbar');
+        if (tabbar !== null) {
+            //The tabbar exists so adjust the content.
+            var content = document.getElementsByClassName('play-content')[0];
+            content.style.height = "calc(100% - 95px)";
+        }
+    }
+
+
+
+    global.playui = {
+        view: {
+            loadView: function(url) {
+
+            }
+        },
+        ajax: function() {
+            var xmlhttp;
+
+            //Prepare the XMLHttpRequest Object for all browsers.
+            if (window.XMLHttpRequest) {
+                // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp = new XMLHttpRequest();
+            } else {
+                // code for IE6, IE5
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+
+
+        }
+    };
+    init();
 }(this));
